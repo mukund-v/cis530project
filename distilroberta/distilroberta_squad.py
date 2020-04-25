8b3e6633cc5517194f2f59c566dfcaf66f1b4201
mukund-v <m17venk@gmail.com>
2020-04-25 12:45:12 -0400

    added model / training files for roberta, distilroberta

---
+++ distilroberta/distilroberta_squad.py
@@ -0,0 +1,59 @@
+import torch
+
+from torch import nn
+from transformers import RobertaModel, RobertaConfig, RobertaTokenizer
+
+class DISTILROBERTA_SQUAD(nn.Module):
+    def __init__(self):
+        super(ROBERTA_SQUAD, self).__init__()
+
+        self.roberta_model = RobertaModel.from_pretrained('distilroberta-base')
+
+        self.fc_layers = nn.Sequential(
+            nn.Linear(768, 256),
+            nn.ReLU(),
+            nn.Linear(256, 2)
+        )
+
+        self.criterion = nn.CrossEntropyLoss()
+
+
+
+    def forward(self, c_q_pairs, attention_mask, token_type_ids, start_indices, end_indices):
+
+        bert_encoded = self.roberta_model(
+            input_ids=c_q_pairs,
+            attention_mask=attention_mask
+        )[0]
+
+        fc_output = self.fc_layers(bert_encoded)
+        start_outputs, end_outputs = fc_output[:, :, 0].squeeze(-1), fc_output[:, :, 1].squeeze(-1)
+
+        start_indices = (start_indices).clamp(0, start_outputs.shape[1]-1)
+        end_indices = (end_indices).clamp(0, start_outputs.shape[1]-1)
+
+        start_loss = self.criterion(start_outputs, start_indices)
+        end_loss = self.criterion(end_outputs, end_indices)
+
+        return 2*start_loss + end_loss
+
+
+    def predict(self, c_q_pairs, attention_mask, token_type_ids):
+        bert_encoded = self.roberta_model(
+            input_ids=c_q_pairs,
+            attention_mask=attention_mask
+        )[0]
+
+        fc_output = self.fc_layers(bert_encoded)
+        start_outputs, end_outputs = fc_output[:, :, 0].squeeze(-1), fc_output[:, :, 1].squeeze(-1)
+
+        starts, s_ind = start_outputs.max(1)
+        ends, e_ind = end_outputs.max(1)
+
+
+        answers = []
+        for i in range(start_outputs.shape[0]):
+            start = s_ind[i].clamp(0, start_outputs.shape[1]-1).item()
+            end = e_ind[i].clamp(0, start_outputs.shape[1]-1).item()
+            answers.append([start, end])
+        return answers
---
+++ roberta/train_roberta.py
@@ -0,0 +1,113 @@
+import json
+import torch
+
+from roberta_squad import ROBERTA_SQUAD
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from transformers import squad_convert_examples_to_features
+from transformers.data.processors.squad import SquadResult, SquadV2Processor
+from transformers import RobertatModel, RobertaConfig, RobertaTokenizer
+
+
+
+device = torch.device('cuda')
+logger = SummaryWriter('logs/roberta_model')
+
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+feature_processor = SquadV2Processor()
+examples = feature_processor.get_train_examples('../data')
+
+features, dataset = squad_convert_examples_to_features(
+    examples=examples,
+    tokenizer=tokenizer,
+    max_seq_length=512,
+    doc_stride=128,
+    max_query_length=128,
+    is_training=True,
+    return_dataset="pt",
+    threads=1
+)
+
+train_loader = DataLoader(dataset=dataset, batch_size=6, shuffle=True)
+rs = ROBERTA_SQUAD().to(device)
+num_epochs = 2
+optimizer = torch.optim.Adam(rs.parameters(), lr=.00003)
+
+for epoch in range(num_epochs):
+  rs.train()
+
+  for i, batch in enumerate(train_loader):
+    c_q_pairs = batch[0].to(device)
+    attention_mask = batch[1].to(device)
+    token_type_ids = batch[2].to(device)
+    start_ind, end_ind = batch[3].to(device), batch[4].to(device)
+
+    optimizer.zero_grad()
+    loss = rs(c_q_pairs, attention_mask, token_type_ids, start_ind, end_ind)
+    logger.add_scalar('loss', epoch * len(train_loader) + i, loss.item())
+    print('loss on batch {} : {}'.format(epoch * len(train_loader) + i, loss.item()))
+    loss.backward()
+    optimizer.step()
+
+    if i % 50 == 0:
+      print('-----------------------------------------------------')
+      print('Results on two random questions from training set : ')
+      for i in range(2):
+        batch = next(iter(dev_loader))
+        c_q_pairs = batch[0].to(device)
+        attention_mask = batch[1].to(device)
+        token_type_ids = batch[2].to(device)
+        start_ind, end_ind = batch[3].to(device), batch[4].to(device)
+        s_ind = start_ind.item()
+        e_ind = end_ind.item()
+
+        rs.eval()
+        indices = rs.predict(c_q_pairs, attention_mask, token_type_ids)
+        start, end = indices[0][0], indices[0][1]
+        print('Context: {} \n'.format(
+            tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(c_q_pairs.view(-1).tolist()))
+            ))
+        print('Answer: {} \n'.format(
+            tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(c_q_pairs.view(-1).tolist())[s_ind:e_ind+1]) if s_ind <= e_ind else None
+        ))
+        print('Predicted answer: {} \n'.format(
+            tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(c_q_pairs.view(-1).tolist())[start:end+1]) if start <= end else None
+        ))
+
+
+      print('-----------------------------------------------------')
+      rs.train()
+
+
+torch.save(rs.state_dict(), './roberta-squad.pt')
+
+examples = feature_processor.get_dev_examples('../data')
+
+rs.eval()
+
+outputs = dict()
+
+for i in range(len(examples)):
+  q_id = examples[i].qas_id
+  context = examples[i].context_text
+  question = examples[i].question_text
+  tokenized = tokenizer.encode_plus(question,
+                               context,
+                               max_length=512,
+                               return_tensors='pt')
+  c_q_pairs = tokenized['input_ids'].to(device)
+  token_type_ids = tokenized['token_type_ids'].to(device)
+  attention_mask = tokenized['attention_mask'].to(device)
+
+  indices = rs.predict(c_q_pairs, attention_mask, token_type_ids)
+  start, end = indices[0][0], indices[0][1]
+  answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(c_q_pairs.view(-1).tolist())[start:end+1]) if start <= end else ""
+  if '[CLS]' in answer:
+    answer = ""
+  outputs[q_id] = answer
+
+  if i % 100 == 0:
+    print('done with example : {}'.format(i))
+
+with open('roberta1-dev-preds.json', 'w') as f:
+  json.dump(outputs, f)
